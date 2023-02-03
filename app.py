3bb0d5ec362759824f6d8a60089ea5ab1f2dae4a
Ram Kripa <ramkripa@Rams-MacBook-Air.local>
2023-02-03 15:58:23 -0800

    flask app seems to work

--- app.py
+++ app.py
@@ -14,7 +14,11 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 auth = Auth()
 solid_connection = SolidAPI(auth)
-web3_connection = None
+web3providers = {
+    "ethereum" : 'https://goerli.infura.io/v3/85e35e212e7c431a838571e469b3c64b',
+    "starknet" : 'https://starknet-goerli.infura.io/v3/85e35e212e7c431a838571e469b3c64b',
+    "avalanche" : 'https://avalanche-fuji.infura.io/v3/85e35e212e7c431a838571e469b3c64b'
+    }
 
 @app.route('/')
 def home():
@@ -54,6 +58,7 @@ def wallet():
     "avalanche" : 'https://avalanche-fuji.infura.io/v3/85e35e212e7c431a838571e469b3c64b'
     }
     chain = "avalanche" # change to an option later
+    session['papayademochain'] = chain
     web3provider = web3providers[chain]
     web3_connection = Web3(HTTPProvider(web3provider))
     web3_connection.middleware_onion.inject(geth_poa_middleware, layer=0)
@@ -85,7 +90,52 @@ def wallet():
             save_wallet_info(private_key, private_wallet_folder + 'private_key.md')
         balance_wei = web3_connection.eth.get_balance(account = wallet_address) # gets balance in wei
         balance = str(Web3.fromWei(balance_wei, 'ether'))
+        session['papayademobalance'] = balance_wei
     else :
         error = "Not able to connect to " + chain + " Network"
     return render_template('wallet.html', name = uname, error = error, balance = balance, new_wallet = new_wallet)
+
+@app.route('/wallet/txn', methods=['POST', 'GET'])
+def txn():
+    error = None
+    msg = None
+    if request.method == 'POST' :
+        try :
+            USERNAME = session['papayademoacctname']
+            chain = session['papayademochain']
+            web3provider = web3providers[chain]
+            web3_connection = Web3(HTTPProvider(web3provider))
+            web3_connection.middleware_onion.inject(geth_poa_middleware, layer=0)
+            wallet_folder = 'https://' + USERNAME + '.solidcommunity.net/public/wallet/' + chain + '/testnet/'
+            private_wallet_folder = 'https://' + USERNAME + '.solidcommunity.net/private/wallet/' + chain + '/testnet/'
+            wallet_address_file_name = wallet_folder + 'wallet_address.md'
+            wallet_address_resp = solid_connection.get(wallet_address_file_name)
+            wallet_address = wallet_address_resp.text
+            private_key_file_name = private_wallet_folder + 'private_key.md'
+            private_key = solid_connection.get(private_key_file_name).text
+            TO_USER = request.form['toUser']
+            AMOUNT = request.form['amountInput']
+            is_signed = request.form['signed']
+            TO_ADDRESS = solid_connection.get('https://' + TO_USER + '.solidcommunity.net/public/wallet/'+ chain + '/testnet/wallet_address.md').text
+            transaction = {
+                    "nonce": web3_connection.eth.get_transaction_count(wallet_address),
+                    "gas": 21000,
+                    "to": TO_ADDRESS,
+                    "value": web3_connection.toWei(AMOUNT, "wei"),
+                    "gasPrice" : 25000000000,
+                    "chainId" : 43113
+            }
+            if is_signed:
+                signed_txn = web3_connection.eth.account.signTransaction(transaction, private_key)
+                txn_hash = web3_connection.eth.sendRawTransaction(signed_txn.rawTransaction)
+                msg = "Transaction processed and the hash is "+ txn_hash.hex()
+            else :
+                msg = "Transaction not Signed"
+        except ZeroDivisionError as e:
+            error = e
+    else :
+        msg = "Welcome to the Transaction Form"
+    return render_template('txn.html', error = error, msg = msg, balance = str(session['papayademobalance']))
+        
+    
     
