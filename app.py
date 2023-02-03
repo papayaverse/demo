0af9d9d908ca6ec93b735b6e635b2fc9ad8c0926
Ram Kripa <ramkripa@Rams-MacBook-Air.local>
2023-02-03 11:37:59 -0800

    basic flask app stuff

--- app.py
+++ app.py
@@ -4,13 +4,21 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def home():
+def home(name=None):
     return render_template(
         "home.html",
-        session=session.get("user"),
-        pretty=json.dumps(session.get("user"), indent=4),
+        name = name,
     )
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
+    error = None
+    if request.method == 'POST':
+        try:
+            IDP = 'https://solidcommunity.net'
+            auth.login(IDP, request.form['username'], request.form['password'])
+            return home(request.form['username'])
+        except :
+            error = 'Invalid username/password' 
+    return render_template('login.html', error=error)
     
     
