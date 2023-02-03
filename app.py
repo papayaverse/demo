a1c4ce8120989d47977f438f66ec310a043ee5e7
Ram Kripa <ramkripa@Rams-MacBook-Air.local>
2023-02-03 12:05:00 -0800

    basic flask login working

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, redirect, render_template, session, url_for, request
+from solid.solid_api import SolidAPI
+from solid.auth import Auth
 
 app = Flask(__name__)
 
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/')
-def home(name=None):
+def home():
+    name = None
+    if 'papayademousername' in session:
+        name = session['papayademousername']
     return render_template(
         "home.html",
         name = name,
@@ -15,10 +21,13 @@ def login():
     if request.method == 'POST':
         try:
             IDP = 'https://solidcommunity.net'
+            auth = Auth()
+            solid_connection = SolidAPI(auth)
             auth.login(IDP, request.form['username'], request.form['password'])
-            return home(request.form['username'])
-        except :
-            error = 'Invalid username/password' 
+            session['papayademousername'] = request.form['username']
+            return redirect(url_for("home"))
+        except Exception as inst:
+            error = inst
     return render_template('login.html', error=error)
     
     
